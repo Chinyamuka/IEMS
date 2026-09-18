b8dc6e8266f2ef0f7caf906d7ebd7b9520063de4
Adon <zambiazactech@gmail.com>
2026-09-18 14:56:59 -0700

    add database configration with Postgresql DB and initialize the connection

--- app/extensions.py
+++ app/extensions.py
@@ -1,6 +1,16 @@
+"""
+IEMS Flask Extensions
+
+This file contains Flask extensions that will be shared
+throughout the application.
+"""
+
+# SQLAlchemy allows us to work with our database using
+# Python classes and objects instead of writing SQL everywhere.
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 
 
+# Create the SQLAlchemy database object.
 db = SQLAlchemy()
 migrate = Migrate()
