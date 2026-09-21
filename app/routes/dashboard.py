d0e7de03ed049ba2380b824ff649e3849c0766f5
Adon <zambiazactech@gmail.com>
2026-09-21 07:54:08 -0700

    add the filter by status on the main dashboard

--- app/routes/dashboard.py
+++ app/routes/dashboard.py
@@ -12,10 +12,18 @@ def index():
     assigned = Equipment.query.filter_by(status="Assigned" ).count()
     under_repair = Equipment.query.filter_by( status="Under Repair").count()
     retired = Equipment.query.filter_by( status="Retired" ).count()
+    status_statistics = [
+        ("In Stock", in_stock),
+        ("Assigned", assigned),
+        ("Under Repair", under_repair),
+        ("Retired", retired),
+    ]
     category_statistics = ( db.session.query(Category.name,  db.func.count(Equipment.id))
                             .join(Equipment,Equipment.category_id == Category.id).group_by( Category.id, Category.name)
                             .order_by(
                             db.func.count(Equipment.id).desc()).all())
+
+
     return render_template(
         "dashboard/index.html",
         total_equipment=total_equipment,
@@ -24,5 +32,6 @@ def index():
         under_repair=under_repair,
         retired=retired,
         category_statistics=category_statistics,
+        status_statistics=status_statistics,
     )
 
